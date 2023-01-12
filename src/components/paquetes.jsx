49ba82f73c2b302295e1c11ab907c425bd1bc25e
AustinBrenesA <116600860+AustinBrenesA@users.noreply.github.com>
2023-01-11 23:26:44 -0600

    Regimen Simplificado

--- src/components/paquetes.jsx
+++ src/components/paquetes.jsx
@@ -2,12 +2,28 @@ import "../Styles/paquetes.css";
 
 function Paquetes (){
     return (
+        <div>
         <div class="Text">
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium hic vero similique impedit corrupti, nihil vel. Quis, magnam magni eos aut nostrum iste provident alias enim itaque nam, corrupti cum?</p>           
             <div class="Paquetes">
                 <h2>PAQUETES</h2>
             </div>
         </div>
+
+        <div class= 'Regimen'>
+            <h2>RÉGIMEN SIMPLIFICADO</h2>
+            <ul>
+                <li>1 Usuario</li>
+                <li>Facturación Iimitada</li>
+                <li>Módulo de ventas</li>
+                <li>Soporte remoto</li>
+                <li>Actualizaciones</li>
+            </ul>
+        </div>
+
+        </div>
+
+        
     )
 }
 
